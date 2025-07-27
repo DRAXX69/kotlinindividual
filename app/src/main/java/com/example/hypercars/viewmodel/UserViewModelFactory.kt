21cf1cdd4ad7aa1af47af82e827809e83d849b86
kritan shrestha <draxx@Draxs-MacBook-Air.local>
2025-07-27 16:20:30 +0545

    user factory

---
+++ app/src/main/java/com/example/hypercars/viewmodel/UserViewModelFactory.kt
@@ -0,0 +1,14 @@
+package com.example.hypercars.viewmodel
+
+import kotlin.jvm.java
+
+class UserViewModelFactory(
+    private val repository: UserRepository
+) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(UserViewModel::class.java)) {
+            return UserViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
