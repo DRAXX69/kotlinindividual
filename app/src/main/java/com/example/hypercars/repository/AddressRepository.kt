99f30e638b0376507ad0b613ce49f6ac29efee3b
kritan shrestha <draxx@Draxs-MacBook-Air.local>
2025-07-27 16:03:39 +0545

    adress repo

---
+++ app/src/main/java/com/example/hypercars/repository/AddressRepository.kt
@@ -0,0 +1,8 @@
+package com.example.hypercars.repository 
+
+interface AddressRepository {
+    fun addAddress(address: AddressModel, callback: (Boolean, String) -> Unit)
+    fun getAddresses(userId: String, callback: (List<AddressModel>) -> Unit)
+    fun updateAddress(address: AddressModel, callback: (Boolean, String) -> Unit)
+    fun deleteAddress(addressId: String, callback: (Boolean, String) -> Unit)
+}
