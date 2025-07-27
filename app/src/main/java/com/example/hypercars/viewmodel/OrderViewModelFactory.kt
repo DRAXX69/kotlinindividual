2fe23e56d2829fe1edf747e9d2041943c7b78fd6
kritan shrestha <draxx@Draxs-MacBook-Air.local>
2025-07-27 16:19:21 +0545

    order factory

---
+++ app/src/main/java/com/example/hypercars/viewmodel/OrderViewModelFactory.kt
@@ -0,0 +1,13 @@
+package com.example.hypercars.viewmodel
+
+import kotlin.jvm.java
+
+class OrderViewModelFactory(private val orderRepository: OrderRepository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(OrderViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return OrderViewModel(orderRepository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
