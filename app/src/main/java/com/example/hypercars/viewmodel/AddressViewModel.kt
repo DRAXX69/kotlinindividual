95b595f5b57f888468ee8cc13ca7ed32b062ff1c
kritan shrestha <draxx@Draxs-MacBook-Air.local>
2025-07-27 16:16:46 +0545

    address view

---
+++ app/src/main/java/com/example/hypercars/viewmodel/AddressViewModel.kt
@@ -0,0 +1,20 @@
+package com.example.hypercars.viewmodel 
+
+class AddressViewModel(private val repository: AddressRepository) : ViewModel() {
+
+    fun addAddress(address: AddressModel, callback: (Boolean, String) -> Unit) {
+        repository.addAddress(address, callback)
+    }
+
+    fun getAddresses(userId: String, callback: (List<AddressModel>) -> Unit) {
+        repository.getAddresses(userId, callback)
+    }
+
+    fun updateAddress(address: AddressModel, callback: (Boolean, String) -> Unit) {
+        repository.updateAddress(address, callback)
+    }
+
+    fun deleteAddress(addressId: String, callback: (Boolean, String) -> Unit) {
+        repository.deleteAddress(addressId, callback)
+    }
+}
