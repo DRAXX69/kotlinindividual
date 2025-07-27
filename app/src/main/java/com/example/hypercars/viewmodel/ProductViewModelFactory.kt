a5f625b81a1569deb753519e782987a715a6f4f6
kritan shrestha <draxx@Draxs-MacBook-Air.local>
2025-07-27 16:19:59 +0545

    product factory

---
+++ app/src/main/java/com/example/hypercars/viewmodel/ProductViewModelFactory.kt
@@ -0,0 +1,15 @@
+package com.example.hypercars.viewmodel
+
+import kotlin.jvm.java
+
+class ProductViewModelFactory(
+    private val repository: ProductRepository
+) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(ProductViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return ProductViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
